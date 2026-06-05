993899bc9d217e15bc0c6181b6570af3e419fa13
Sae Nakatani <nakatanisae0218@gmail.com>
2026-06-05 08:49:03 -0700

    Exercise 35 completed ( 14 / 22 )

--- exercises/ex35.js
+++ exercises/ex35.js
@@ -10,7 +10,17 @@
 const _ = require('lodash');
 const products = require('../data/products.json');
 
-const lodashSolution = null;
+const lodashSolution = _.chain(products)
+  .filter((product) => product.stock < 30)
+  .map((product) => {
+    return {
+      name: product.name,
+      category: product.category,
+      stock: product.stock
+    }
+  })
+  .orderBy(['stock', 'name'],['asc', 'asc'])
+  .value()
 
 console.log(lodashSolution);
 
